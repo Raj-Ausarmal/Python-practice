10bd783ac3a43fd6444c424840e6ad6930fd0c78
Raj-Ausarmal <rajausarmal@gmail.com>
2026-08-24 21:22:44 +0530

    Organize chapter 2 files

--- 05_type.py
+++
@@ -1,5 +0,0 @@
-a="31.2"
-b=float(a)
-t=type(b)
-print(t)
-
--- 06_input.py
+++
@@ -1,8 +0,0 @@
-a = int(input("number 1:"))
-
-b = int(input("number 2:"))
-
-print("number a is ",a)
-print("number a is ",b)
-
-print ("sum is", a+b)
