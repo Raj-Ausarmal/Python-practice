c726a04f10c75dcfba0a4b8b0037e16503e31c6e
Raj-Ausarmal <rajausarmal@gmail.com>
2026-08-27 12:45:41 +0530

    Add conditionals practice

---
+++ 06_conditionals/chapter 6/01_conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("enter your age : "))
+
+if a>=18:
+ print("your age is above consent\nand thats good")
+
+elif a<0:
+ print("dont enter negative age atleast enter the age in positive") 
+
+else:
+ print("you are under aged") 
---
+++ 06_conditionals/chapter 6/02_if_elif_else_ladder.py
@@ -0,0 +1,18 @@
+a = int(input("enter your age : "))
+
+#If elif else ladder
+
+if a>=18:
+ print("your age is above consent\nand thats good")
+
+elif a<0:
+ print("dont enter negative age atleast enter the positive age") 
+
+elif a==0:
+ print("entering zero is invalid")  
+
+else:
+ print("you are under aged") 
+
+
+print("end of programm")
